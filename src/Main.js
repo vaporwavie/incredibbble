ad31b14693fa105f812e26d4605ffdd5bf7a6e11
Luiz Nickel <luizaugustonickel@gmail.com>
2017-12-04 17:22:55 -0300

    Adds SASS, React (DOM also) and a Main class

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Incredibbble</h1>
+                <ul className="menu">
+                    <li>Home</li>
+                    <li>Search</li>
+                    <li>About</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello react");
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+
+ReactDOM.render(
+  <Main/>,
+  document.getElementById("root")
+);
