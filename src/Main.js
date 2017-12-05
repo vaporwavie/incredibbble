8005bd014877048316d8de60877af3703c722790
Luiz Nickel <luizaugustonickel@gmail.com>
2017-12-04 21:47:41 -0300

    Adds react router into the main javascript file & changes React behaviour and adds two more pages to the project.

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,33 @@
 import React, {Component} from "react";
 
-class Main extends Component {
+import {Route, NavLink, HashRouter} from "react-router-dom";
+
+import Search from "./Search";
+import About from "./About";
+
+class Main extends React.Component {
     render() {
         return (
-            <div>
-                <ul className="menu">
-                    <h1>Incredibbble</h1>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Search</a>
-                    </li>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                </ul>
-            </div>
+            <HashRouter>
+                <div>
+                    <ul className="menu">
+                        <h1>Incredibbble</h1>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/search">Search</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                    </ul>
+                    <div className="content">
+                        <Route path="/search" component={Search}/>
+                        <Route path="/about" component={About}/>
+                    </div>
+                </div>
+            </HashRouter>
         )
     }
 }
--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-class Search extends Component {
+class Search extends React.Component {
     render() {
         return (
             <div>
