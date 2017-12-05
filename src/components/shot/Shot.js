7b526e4688e8b0090d087488c429a929692ac42a
Luiz Nickel <luizaugustonickel@gmail.com>
2017-12-05 18:20:04 -0300

    Adds overlay mockup for the shot detail

--- src/components/shot/Shot.js
+++ src/components/shot/Shot.js
@@ -4,7 +4,16 @@ import "./styles/shot.css"
 
 const Shot = props => (
     <li className="shot">
-        <img src={props.url} />
+        <img src={props.url}/>
+        <a href="#"><div className="details">
+            <p>Details for this shot</p>
+            <p>More details right here</p>
+            <p>More details right here</p>
+            <p>More details right here</p>
+            <p>More details right here</p>
+            <p>More details right here</p>
+        </div>
+        </a>
     </li>
 );
 
